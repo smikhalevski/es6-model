76945529d0da6d0390ae20f9d4a6e5d579652f01
Savva Mikhalevski <smikhalevski@gmail.com>
2016-02-02 23:57:57 +0300

    Added `EventDispatcher` + tests

---
+++ src/main/js/EventDispatcher.js
@@ -0,0 +1,105 @@
+export const LISTENERS = Symbol('listeners');
+
+/**
+ * @class EventDispatcher
+ * @classdesc
+ * Gives the ability to manage and dispatch custom named events.
+ */
+export default class EventDispatcher {
+
+  constructor () {
+    this[LISTENERS] = new Map;
+  }
+
+  /**
+   * Register an event listener of a specific event type.
+   *
+   * @method
+   * @name EventDispatcher#addEventListener
+   * @param {...Function} classes Classes of events to add listener to.
+   * @param {Function} listener Listener that receives a notification when
+   *        an event of the specified type is being dispatched.
+   */
+  addEventListener (...classes) {
+    let listener = classes.pop();
+    if (listener instanceof Function) {
+      for (let Class of classes) {
+        if (Class instanceof Function) {
+          let set = this[LISTENERS].get(Class);
+          if (set) {
+            set.add(listener);
+          } else {
+            this[LISTENERS].set(Class, new Set([listener]));
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Removes an event listener.
+   *
+   * If no event types are provided, listener is removed from all types
+   * of events registered for object at runtime.
+   *
+   * @method
+   * @name EventDispatcher#removeEventListener
+   * @param {...Function} [classes] Classes of events to remove listener from.
+   * @param {Function} listener Listener to remove from the event target.
+   */
+  removeEventListener (...classes) {
+    let listener = classes.pop();
+    if (classes.length === 0) {
+      classes = this[LISTENERS].keys();
+    }
+    for (let Type of classes) {
+      let set = this[LISTENERS].get(Type);
+      if (set) {
+        set.delete(listener);
+      }
+    }
+  }
+
+  /**
+   * Dispatch an event notifying listeners appropriate for provided event.
+   *
+   * @method
+   * @name EventDispatcher#dispatchEvent
+   * @param {Object} event Event to dispatch.
+   */
+  dispatchEvent (event) {
+    let listeners = [];
+    for (let Class of this[LISTENERS].keys()) {
+      if (event instanceof Class) {
+        for (let listener of this[LISTENERS].get(Class)) {
+          if (listeners.includes(listener)) {
+            continue; // Listener has been invoked during dispatch of this event.
+          }
+          listener.call(this, event);
+          listeners.push(listener);
+        }
+      }
+    }
+  }
+
+  /**
+   * Invokes callback in transaction, causing events to wait until callback finishes,
+   * and dispatches them on successful competition.
+   *
+   * @method
+   * @name EventDispatcher#transaction
+   * @param {Function} callback Transaction callback.
+   */
+  transaction (callback) {
+    let events = [];
+    this.dispatchEvent = event => events.push(event);
+    try {
+      callback.call(this);
+    } finally {
+      delete this.dispatchEvent;
+    }
+    for (let event of events) {
+      this.dispatchEvent(event);
+    }
+  }
+}
---
+++ src/test/js/EventDispatcherTest.js
@@ -0,0 +1,207 @@
+import 'babel-polyfill';
+import assert, {equal, deepEqual} from 'assert';
+import EventDispatcher, {LISTENERS} from '../../main/js/EventDispatcher';
+
+describe('EventDispatcher', () => {
+
+  it('uses private properties', () => {
+    equal(JSON.stringify(new EventDispatcher), '{}');
+  });
+});
+
+describe('EventDispatcher#addEventEventListener', () => {
+
+  function listener1 () {}
+  function listener2 () {}
+
+  class TestEvent1 {}
+  class TestEvent2 {}
+
+  it('adds listener to event type', () => {
+    let target = new EventDispatcher;
+    target.addEventListener(TestEvent1, listener1);
+
+    equal(Array.from(target[LISTENERS].get(TestEvent1)).indexOf(listener1), 0);
+  });
+
+  it('does not add same listener twice', () => {
+    let target = new EventDispatcher;
+    target.addEventListener(TestEvent1, listener1);
+    target.addEventListener(TestEvent1, listener2);
+    target.addEventListener(TestEvent1, listener1);
+
+    equal(Array.from(target[LISTENERS].get(TestEvent1)).indexOf(listener1), 0);
+    equal(Array.from(target[LISTENERS].get(TestEvent1)).indexOf(listener2), 1);
+  });
+
+  it('can add listener to multiple event types', () => {
+    let target = new EventDispatcher;
+    target.addEventListener(TestEvent1, TestEvent2, listener1);
+
+    equal(Array.from(target[LISTENERS].get(TestEvent1)).indexOf(listener1), 0);
+    equal(Array.from(target[LISTENERS].get(TestEvent2)).indexOf(listener1), 0);
+  });
+});
+
+describe('EventDispatcher#removeEventListener', () => {
+
+  function listener1 () {}
+  function listener2 () {}
+
+  class TestEvent1 {}
+  class TestEvent2 {}
+  class TestEvent3 {}
+
+  it('removes listener from event type', () => {
+    let target = new EventDispatcher;
+    target.addEventListener(TestEvent1, listener1);
+    target.addEventListener(TestEvent1, listener2);
+    target.removeEventListener(TestEvent1, listener1);
+
+    equal(target[LISTENERS].get(TestEvent1).size, 1);
+    equal(Array.from(target[LISTENERS].get(TestEvent1)).indexOf(listener2), 0);
+  });
+
+  it('can remove listener from multiple event types', () => {
+    let target = new EventDispatcher;
+    target.addEventListener(TestEvent1, TestEvent2, TestEvent3, listener1);
+    target.removeEventListener(TestEvent1, TestEvent2, listener1);
+
+    equal(target[LISTENERS].get(TestEvent1).size, 0);
+    equal(target[LISTENERS].get(TestEvent2).size, 0);
+    equal(target[LISTENERS].get(TestEvent3).size, 1);
+  });
+
+  it('can remove listener from all event types', () => {
+    let target = new EventDispatcher;
+    target.addEventListener(TestEvent1, TestEvent2, listener1);
+    target.removeEventListener(listener1);
+
+    equal(target[LISTENERS].get(TestEvent1).size, 0);
+    equal(target[LISTENERS].get(TestEvent2).size, 0);
+  });
+
+  it('does not affect dispatch', () => {
+    var trace = [];
+    function listener1 () {
+      trace.push(listener1);
+      this.removeEventListener(listener1);
+    }
+    function listener2 () {
+      trace.push(listener2);
+    }
+    let target = new EventDispatcher;
+    target.addEventListener(TestEvent1, listener1);
+    target.addEventListener(TestEvent1, listener2);
+
+    target.dispatchEvent(new TestEvent1);
+    deepEqual(trace, [listener1, listener2]);
+  });
+});
+
+describe('EventDispatcher#dispatchEvent', () => {
+
+  class TestEvent1 {}
+  class TestEvent2 {}
+  class TestEvent3 extends TestEvent1 {}
+
+  it('invokes listeners for an event by its type', () => {
+    var trace = [];
+    function listener1 () {
+      trace.push(listener1);
+    }
+    function listener2 () {
+      trace.push(listener2);
+    }
+    function listener3 () {
+      trace.push(listener3);
+    }
+    let target = new EventDispatcher;
+    target.addEventListener(TestEvent1, listener1);
+    target.addEventListener(TestEvent1, listener2);
+    target.addEventListener(TestEvent2, listener3);
+
+    target.dispatchEvent(new TestEvent1);
+    deepEqual(trace, [listener1, listener2]);
+
+    target.dispatchEvent(new TestEvent2);
+    deepEqual(trace, [listener1, listener2, listener3]);
+  });
+
+  it('looks-up event listener by `instanceof`', () => {
+    var trace = [];
+    function listener1 () {
+      trace.push(listener1);
+    }
+    function listener2 () {
+      trace.push(listener2);
+    }
+    let target = new EventDispatcher;
+    target.addEventListener(TestEvent1, listener1);
+    target.addEventListener(TestEvent3, listener2);
+
+    target.dispatchEvent(new TestEvent3);
+    deepEqual(trace, [listener1, listener2]);
+  });
+
+  it('does not fire same listener multiple times during single dispatch', done => {
+    function listener () {
+      done();
+    }
+    let target = new EventDispatcher;
+    target.addEventListener(TestEvent1, listener);
+    target.addEventListener(TestEvent3, listener);
+
+    target.dispatchEvent(new TestEvent3);
+  });
+});
+
+describe('EventDispatcher#transaction', () => {
+
+  class TestEvent1 {}
+  class TestEvent2 {}
+
+  it('invokes callback and dispatches events only after completion', () => {
+    let target = new EventDispatcher,
+        flag1 = false,
+        flag2 = false,
+        listenerCallCount = 0;
+
+    // Both of these listeners should see both flags to have updated value.
+    function listener1 () {
+      equal(flag1, true);
+      equal(flag2, true);
+      listenerCallCount++;
+    }
+    function listener2 () {
+      equal(flag1, true);
+      equal(flag2, true);
+      listenerCallCount++;
+    }
+
+    target.addEventListener(TestEvent1, listener1);
+    target.addEventListener(TestEvent2, listener2);
+
+    target.transaction(() => {
+      flag1 = true;
+      target.dispatchEvent(new TestEvent1);
+      flag2 = true;
+      target.dispatchEvent(new TestEvent2);
+    });
+
+    equal(listenerCallCount, 2);
+    equal(target.dispatchEvent, EventDispatcher.prototype.dispatchEvent);
+  });
+
+  it('reverts `dispatchEvent` to initial state on callback failure', done => {
+    let target = new EventDispatcher;
+    try {
+      target.transaction(() => {
+        throw new Error('Expected error');
+      });
+    } catch (e) {
+      equal(target.dispatchEvent, EventDispatcher.prototype.dispatchEvent);
+      done();
+    }
+  });
+});
