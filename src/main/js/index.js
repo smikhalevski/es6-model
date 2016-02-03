33487a2c9fbaa2da9682b4558b7d14a18c0f5c34
Savva Mikhalevski <smikhalevski@gmail.com>
2016-02-03 17:02:00 +0300

    Library bundle includes all classes

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,5 +1,12 @@
-import Model from './Model';
-import EventDispatcher from './EventDispatcher';
+import AddEvent from './AddEvent';
+import AttributeDescriptor from './AttributeDescriptor';
 import ChangeEvent from './ChangeEvent';
+import EventDispatcher from './EventDispatcher';
+import index from './index';
+import List from './List';
+import Model from './Model';
+import MutationEvent from './MutationEvent';
+import RemoveEvent from './RemoveEvent';
+import SortEvent from './SortEvent';
 
-export default {Model, EventDispatcher, ChangeEvent};
+export default {AddEvent, AttributeDescriptor, ChangeEvent, EventDispatcher, List, Model, MutationEvent, RemoveEvent, SortEvent};
